4995900d7b59cd6937db60c234f847f4d1b966b9
Taylor <triccetti@cettiworks.com>
2017-12-10 02:00:02 -0800

    working commit

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,10 +1,12 @@
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -18,11 +20,17 @@ import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.jnlp.FileSaveService;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import java.util.stream.Stream;
+
+import static java.lang.Thread.sleep;
 
 
 public class MainGUI extends Application {
@@ -30,12 +38,12 @@ public class MainGUI extends Application {
     /**
      * The scenes height.
      */
-    private static final int HEIGHT = 600;
+    private static final int HEIGHT = 800;
 
     /**
      * The scenes width.
      */
-    private static final int WIDTH = 800;
+    private static final int WIDTH = 600;
 
     private final int ROW_HEIGHT = 24;
 
@@ -45,6 +53,8 @@ public class MainGUI extends Application {
     private Stage stage;
 
     private final ObservableList data = FXCollections.observableArrayList();
+
+    private ListView listView;
 
     private ArrayList<File> filesToTrain;
 
@@ -74,35 +84,31 @@ public class MainGUI extends Application {
         startPane.setPadding(new Insets(ROW_HEIGHT));
         startPane.setOrientation(Orientation.VERTICAL);
         startPane.setAlignment(Pos.TOP_CENTER);
-        ListView listView = new ListView(data);
+        listView = new ListView(data);
 
         Button addTrainingText = new Button("Upload Text");
         listView.setPrefHeight(1);
 
+        Button trainText = new Button("Train");
+
         addTrainingText.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                Text t = getTrainingFile();
+                List<Text> t = getTrainingFile();
+                trainText.setDisable(false);
                 if (t != null) {
-                    data.add(t);
-                    listView.prefHeightProperty().bind(Bindings.size(data).multiply(ROW_HEIGHT));
+                    for(Text text : t) {
+                        data.add(text);
+                    }
+                    listView.setPrefHeight(data.size() * ROW_HEIGHT);
                 }
             }
         });
 
-        Button trainText = new Button("Train");
-        trainText.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                trainText.setText("Training...");
-                storyGen.train(filesToTrain);
-                trainText.setText("Train");
-            }
-        });
 
-        TextArea wordsInStory = new TextArea("1000");
+        TextArea wordsInStory = new TextArea("100");
         wordsInStory.setPrefWidth(ROW_HEIGHT * 2);
-        wordsInStory.setPrefHeight(ROW_HEIGHT / 2);
+        wordsInStory.setPrefHeight(ROW_HEIGHT - 20);
 
         // force the field to be numeric only
         wordsInStory.textProperty().addListener(new ChangeListener<String>() {
@@ -115,21 +121,58 @@ public class MainGUI extends Application {
             }
         });
 
+        Text message = new Text();
+
+        Task<Void> longRunningTask = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+
+                long startTime = System.currentTimeMillis();
+                storyGen.loadFiles(filesToTrain);
+                storyGen.train();
+                long endTime = System.currentTimeMillis();
+
+                long duration = (endTime - startTime) / 1000L;
+                message.setText(message.getText() + " It took " + duration + " seconds to train the Story Generator.");
+
+                Platform.runLater(() -> trainText.setText("Train Complete"));
+                return null;
+            }
+        };
+
 
         TextArea storyResults = new TextArea();
         storyResults.setEditable(false);
         storyResults.setWrapText(true);
+        storyResults.setPrefHeight(ROW_HEIGHT * 15);
         ScrollPane sp = new ScrollPane();
         sp.setContent(storyResults);
         sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
         sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
 
+        trainText.setOnAction(e -> {
+            if (!storyGen.isTraining()) {
+                trainText.setText("Training...");
+                Calendar rightNow = Calendar.getInstance();
+                String now = rightNow.get(Calendar.HOUR_OF_DAY) + ":" + rightNow.get(Calendar.MINUTE);
+                message.setText("Started training at " + now);
+                trainText.setDisable(true);
+                new Thread(longRunningTask).start();
+
+            }
+        });
+
+
         Button generateStory = new Button("Generate a Story");
         generateStory.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                String story = storyGen.generateStory(Integer.parseInt(wordsInStory.getText()));
-                storyResults.setText(story);
+                if (!storyGen.isTrainingComplete()) {
+                    storyResults.setText("Training is not complete.");
+                } else {
+                    String story = storyGen.generateStory(Integer.parseInt(wordsInStory.getText()));
+                    storyResults.setText(story);
+                }
             }
         });
         generateStory.setAlignment(Pos.CENTER);
@@ -141,6 +184,21 @@ public class MainGUI extends Application {
             public void handle(ActionEvent e) {
                 String story = storyResults.getText();
 
+                File workingDirectory = new File(System.getProperty("user.dir"));
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setInitialDirectory(workingDirectory);
+                fileChooser.setTitle("Save your story.");
+                File fileToSave = fileChooser.showSaveDialog(stage);
+                if(fileToSave != null) {
+                    try {
+                        Files.write(fileToSave.toPath(), story.getBytes());
+                    } catch (Exception err) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR,
+                                "Your story could not be saved please try again.");
+                        alert.showAndWait();
+                    }
+                }
+
             }
         });
         saveStory.setAlignment(Pos.CENTER);
@@ -151,7 +209,7 @@ public class MainGUI extends Application {
         trainButtons.setHgap(10);
 
         trainButtons.add(addTrainingText, 0, 0);
-        trainButtons.add(trainText, 1,0);
+        trainButtons.add(trainText, 1, 0);
 
         GridPane genButtons = new GridPane();
         genButtons.setPadding(new Insets(ROW_HEIGHT));
@@ -159,34 +217,45 @@ public class MainGUI extends Application {
         genButtons.add(new Label("Number of words in the story: "), 0, 0);
         genButtons.add(wordsInStory, 1, 0);
         genButtons.add(generateStory, 2, 0);
-        startPane.getChildren().addAll(listView, new Text("Click file to delete."), trainButtons, genButtons,
-                storyResults, saveStory);
+        startPane.getChildren().addAll(listView, new Text("Click file to delete."), trainButtons, message,
+                genButtons, storyResults, saveStory);
 
 
         return startPane;
     }
 
-    public Text getTrainingFile() {
+
+    public  List<Text> getTrainingFile() {
         File workingDirectory = new File(System.getProperty("user.dir"));
         FileChooser fileChooser = new FileChooser();
         fileChooser.setInitialDirectory(workingDirectory);
         fileChooser.setTitle("Open Resource File");
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-        File selectedFile = fileChooser.showOpenDialog(stage);
-        if (selectedFile != null) {
-            filesToTrain.add(selectedFile);
+        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(stage);
 
-            Text fileToTrainWith = new Text(selectedFile.getName());
-            fileToTrainWith.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    // If the file is clicked, remove it
-                    data.remove(fileToTrainWith);
-                    filesToTrain.remove(selectedFile);
-                }
-            });
+        if (selectedFiles != null) {
+            List<Text> texts = new ArrayList<>();
+            for (File selectedFile : selectedFiles) {
+                filesToTrain.add(selectedFile);
 
-            return fileToTrainWith;
+                Text fileToTrainWith = new Text(selectedFile.getName());
+
+                fileToTrainWith.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        // If the file is clicked, remove it
+                        data.remove(fileToTrainWith);
+                        filesToTrain.remove(selectedFile);
+                        listView.setPrefHeight(data.size() * ROW_HEIGHT);
+
+                    }
+                });
+
+                texts.add(fileToTrainWith);
+
+            }
+            return texts;
+
         }
         return null;
     }
--- src/StoryGenerator.java
+++ src/StoryGenerator.java
@@ -1,28 +1,95 @@
+import com.sun.org.apache.xpath.internal.operations.Equals;
+
 import java.io.File;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.stream.Stream;
+import java.util.*;
 
 public class StoryGenerator {
 
+    private LinkedList<String> words;
+
+    private HashMap<String, ArrayList<String>> allTriagrams;
+
+    private boolean filesLoaded;
+    private boolean trained;
+    private boolean training;
+
     public StoryGenerator() {
-        // empty
+        filesLoaded = false;
+        trained = false;
+        training = false;
     }
 
-    public void train(ArrayList<File> files) {
-        for(File f : files) {
-            //read file into stream, try-with-resources
-            try (Stream<String> stream = Files.lines(f.toPath())) {
-                stream.forEach(System.out::println);
-            }  catch (Exception error) {
-                // error
+    public void loadFiles(ArrayList<File> files) {
+        words = new LinkedList<>();
+        boolean loaded = true;
+        for (File f : files) {
+            // Read in the file with words in order.
+            Scanner sc;
+            try {
+                sc = new Scanner(f);
+                while (sc.hasNext()) {
+                    String line = sc.next();
+                    words.add(line.toLowerCase());
+                }
+            } catch (Exception e) {
+                loaded = false;
+                System.err.println("The file " + f.getName() + " could not be read.");
             }
+        }
+        filesLoaded = loaded;
+    }
+
+    public void train() {
+        if (filesLoaded) {
+            allTriagrams = new HashMap<>();
+            training = true;
+            for (int i = 0; i < words.size() - 2; i++) {
+
+                // triagrams.add(new Triagram(words.get(i), words.get(i + 1), words.get(i + 2)));
+                String bigram = words.get(i) + " " + words.get(i + 1);
+                String w3 = words.get(i + 2);
+                if (allTriagrams.containsKey(bigram)) {
+                    // adds the third word to the list of words
+                    allTriagrams.get(bigram).add(w3);
+                } else {
+                    ArrayList<String> list = new ArrayList<>();
+                    list.add(w3);
+                    allTriagrams.put(bigram, list);
+                }
+            }
+            trained = true;
+            training = false;
+            filesLoaded = false;
         }
     }
 
     public String generateStory(int numWords) {
+        StringBuilder story = new StringBuilder();
+        if (trained) {
+            int startWordIndex = (int) (Math.random() * words.size() - 1);
+            String w1 = words.get(startWordIndex);
+            String w2 = words.get(startWordIndex + 1);
 
-        return "<Insert story here>";
+            for (int i = 0; i < numWords; i++) {
+                story.append(w1);
+                story.append(" ");
+                String value = w1 + " " + w2;
+                ArrayList<String> list = allTriagrams.get(value);
+                w1 = w2;
+                w2 = list.get((int) (Math.random() * list.size()));
+            }
+        } else {
+            story.append("StoryGenerator is not trained. Please train on some text.");
+        }
+        return story.toString().trim();
+    }
+
+    public boolean isTraining() {
+        return training;
+    }
+
+    public boolean isTrainingComplete() {
+        return trained;
     }
 
 }
