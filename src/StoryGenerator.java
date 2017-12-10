c8cbbdf27e39fe3edca08664ecf76ca44fb75ced
Taylor <triccetti@cettiworks.com>
2017-12-10 13:58:24 -0800

    Final Version

--- src/StoryGenerator.java
+++ src/StoryGenerator.java
@@ -1,26 +1,74 @@
-import com.sun.org.apache.xpath.internal.operations.Equals;
-
 import java.io.File;
 import java.util.*;
 
+/**
+ * The StoryGenerator uses a Trigram language model to
+ * generate stories. You need to load files for the StoryGenerator
+ * to read and then train the StoryGenerator. After training is complete
+ * you can generate a story with the number of words you want.
+ *
+ * TCSS 435 - AI
+ * Programming assignment 3
+ * 
+ * @Author Taylor Riccetti
+ */
 public class StoryGenerator {
 
-    private LinkedList<String> words;
+    /**
+     * Console user input for the StoryGenerator.
+     *
+     * @param args command line arguments.
+     */
+    public static void main(String[] args) {
+        StoryGenerator storyGen = new StoryGenerator();
+        Scanner input = new Scanner(System.in);
+        System.out.println("============= WELCOME TO THE STORY GENERATOR ==============");
+        System.out.print("Please enter files you would like to train the generator on (Seperate them by ','): ");
+        String[] listOfFiles = input.nextLine().split(",");
+        ArrayList<File> files = new ArrayList<>();
+        for (String fileName : listOfFiles) {
+            files.add(new File(fileName));
+        }
+        System.out.println("We are traing the files. Please be aware this may take a few minuets.");
+        storyGen.loadFiles(files);
+        storyGen.train();
+        System.out.print("Training has completed. To generate a story we need the number of word you would like to have: ");
+        int count = input.nextInt();
+        System.out.println("You have generated a " + count + " word story!");
+        System.out.println("=============================================================================");
+        System.out.println(storyGen.generateStory(count));
+    }
 
-    private HashMap<String, ArrayList<String>> allTriagrams;
+    /**
+     * The array list of words in order.
+     */
+    private ArrayList<String> words;
+
+    /**
+     * The list of trigrams.
+     */
+    private HashMap<String, ArrayList<String>> allTrigrams;
 
     private boolean filesLoaded;
     private boolean trained;
     private boolean training;
 
+    /**
+     * A Story generator object that handles generating stories.
+     */
     public StoryGenerator() {
         filesLoaded = false;
         trained = false;
         training = false;
     }
 
+    /**
+     * Takes in a list of files and creates an in-order list of words.
+     *
+     * @param files the files to read in.
+     */
     public void loadFiles(ArrayList<File> files) {
-        words = new LinkedList<>();
+        words = new ArrayList<>();
         boolean loaded = true;
         for (File f : files) {
             // Read in the file with words in order.
@@ -39,22 +87,24 @@ public class StoryGenerator {
         filesLoaded = loaded;
     }
 
+    /**
+     * Trains the program on the list of words and creates a trigram
+     * map of bigrams and the 3rd word that follows them.
+     */
     public void train() {
         if (filesLoaded) {
-            allTriagrams = new HashMap<>();
+            allTrigrams = new HashMap<>();
             training = true;
             for (int i = 0; i < words.size() - 2; i++) {
-
-                // triagrams.add(new Triagram(words.get(i), words.get(i + 1), words.get(i + 2)));
                 String bigram = words.get(i) + " " + words.get(i + 1);
                 String w3 = words.get(i + 2);
-                if (allTriagrams.containsKey(bigram)) {
-                    // adds the third word to the list of words
-                    allTriagrams.get(bigram).add(w3);
+                if (allTrigrams.containsKey(bigram)) {
+                    allTrigrams.get(bigram).add(w3);
                 } else {
+                    // Adds words to an arraylist to account for the frequency of the word.
                     ArrayList<String> list = new ArrayList<>();
                     list.add(w3);
-                    allTriagrams.put(bigram, list);
+                    allTrigrams.put(bigram, list);
                 }
             }
             trained = true;
@@ -63,6 +113,13 @@ public class StoryGenerator {
         }
     }
 
+    /**
+     * Takes in the number of words you want in your story
+     * and generates a story based on the text you trained on.
+     *
+     * @param numWords the number of words in your story.
+     * @return a story.
+     */
     public String generateStory(int numWords) {
         StringBuilder story = new StringBuilder();
         if (trained) {
@@ -74,20 +131,31 @@ public class StoryGenerator {
                 story.append(w1);
                 story.append(" ");
                 String value = w1 + " " + w2;
-                ArrayList<String> list = allTriagrams.get(value);
+                ArrayList<String> list = allTrigrams.get(value);
                 w1 = w2;
                 w2 = list.get((int) (Math.random() * list.size()));
             }
         } else {
-            story.append("StoryGenerator is not trained. Please train on some text.");
+            story.append("StoryGenerator is not trained. Please train some text files.");
         }
         return story.toString().trim();
     }
 
+    /**
+     * Returns true if the StoryGenerator is currently
+     * training or not.
+     *
+     * @return if the StoryGenerator is training.
+     */
     public boolean isTraining() {
         return training;
     }
 
+    /**
+     * Returns true if the StoryGenerator is trained or not.
+     *
+     * @return if the story generator is trained.
+     */
     public boolean isTrainingComplete() {
         return trained;
     }
